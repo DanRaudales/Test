f349a00b030346a198b74c2cf5b43184fc40ab81
Daniel Raudales <ska-rnales95@hotmail.com>
2018-04-24 23:08:11 -0500

    Web view

--- testing/src/pages/home/home.ts
+++ testing/src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,57 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  map: any;
+  data: any;
+  estado: any;
+  latitude: any;
+  longitude: any;
 
+  constructor(
+    public navCtrl: NavController,
+    private geo: Geolocation,
+    private platform: Platform
+  ) {
+    this.platform.ready().then(() => {
+    
+      var options = {
+        timeout: 5000
+      };
+
+      this.geo.getCurrentPosition(options).then(resp => {
+        console.log(resp.coords.latitude);
+        console.log(resp.coords.longitude);
+        this.latitude = resp.coords.latitude;
+        this.longitude = resp.coords.longitude;
+      }).catch(() => {
+        console.log("Error to get location");
+      });
+
+    });
+  }
+
+  ngOnInit():void{
+    this.drawMap();
+  }
+
+  drawMap(): void {
+    this.map = Leaflet.map('map').fitWorld();
+    Leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'AppTuto',
+      maxZoom: 18
+    }).addTo(this.map);
+    this.map.locate({
+      setView: true,
+      maxZoom: 12
+    }).on('locationfound', (e) => {
+      let markerGroup = Leaflet.featureGroup();
+      var latLang = [this.latitude, this.longitude];
+      let marker: any = Leaflet.marker(latLang).on('click', () => {
+        alert('Tú estás aquí');
+      });
+      markerGroup.addLayer(marker);
+      this.map.addLayer(markerGroup);
+    });
   }
 
 }
