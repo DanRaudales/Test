d5ac0c2e2c756d828a629517c6826a3d2178fad3
Unknown <odiazcorona@gmail.com>
2018-05-03 14:17:22 -0500

    Marcadores de escuelas
    Se añadieron marcadores de UPIICSA, ESCOM y UPIITA.

--- testing/src/pages/home/home.ts
+++ testing/src/pages/home/home.ts
@@ -15,13 +15,16 @@ export class HomePage {
   estado: any;
   latitude: any;
   longitude: any;
+  upiicsa: any;
+  escom: any;
+  upiita: any;
 
   constructor(
     public navCtrl: NavController,
     private geo: Geolocation,
     private platform: Platform
   ) {
-    this.platform.ready().then(() => {
+  this.platform.ready().then(() => {
     
       var options = {
         timeout: 5000
@@ -32,35 +35,34 @@ export class HomePage {
         console.log(resp.coords.longitude);
         this.latitude = resp.coords.latitude;
         this.longitude = resp.coords.longitude;
-      }).catch(() => {
-        console.log("Error to get location");
+      }).catch((error) => {
+        console.log("Error to get location ", error);
       });
 
     });
   }
 
   ngOnInit():void{
+    this.upiicsa = [19.395893, -99.092330];
+    this.escom = [19.504537, -99.146931];
+    this.upiita = [19.511614, -99.126188];
     this.drawMap();
   }
 
   drawMap(): void {
-    this.map = Leaflet.map('map').fitWorld();
+    
+    this.map = Leaflet.map('map').setView([19.3910038,-99.2837002], 11);
     Leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: 'AppTuto',
       maxZoom: 18
     }).addTo(this.map);
-    this.map.locate({
-      setView: true,
-      maxZoom: 12
-    }).on('locationfound', (e) => {
-      let markerGroup = Leaflet.featureGroup();
-      var latLang = [this.latitude, this.longitude];
-      let marker: any = Leaflet.marker(latLang).on('click', () => {
-        alert('Tú estás aquí');
-      });
-      markerGroup.addLayer(marker);
-      this.map.addLayer(markerGroup);
-    });
+    Leaflet.marker(this.upiicsa).addTo(this.map).bindPopup('UPIICSA');
+    Leaflet.marker(this.escom).addTo(this.map).bindPopup('ESCOM');
+    Leaflet.marker(this.upiita).addTo(this.map).bindPopup('<a href = "https://www.upiita.ipn.mx/" target = "_blank">UPIITA</a>');
+  }
+
+  details(a){
+    alert(a);
   }
 
 }
