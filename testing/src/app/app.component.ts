b5aedf53030a370e5d4e64f7acfe1745461537be
Unknown <oskr4395@gmail.com>
2018-09-12 15:44:43 -0500

    Favoritos
    Abre favoritos y pasa parametros, comente unas lineas que no se que hacen, y se cambio el Array de favoritos

--- testing/src/app/app.component.ts
+++ testing/src/app/app.component.ts
@@ -1,24 +1,46 @@
-import { Component } from '@angular/core';
-import { Platform, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, NavController, NavParams, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { timer } from 'rxjs/observable/timer';
-import { FavoritesPage } from '../pages/favorites/favorites';
 
+import { FavoritesPage } from '../pages/favorites/favorites';
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = HomePage;
+  favorito:any = FavoritesPage;
 
-  favoritos: any[] = ["4tools Power Systems", "C-ESTRATEGIA","SITAH","PTREE","PRO PHARMA RESEARCH","EL REFORMA","IFT","DIARIO DE MEXICO","OXXO","7 ELEVEN","PLAYGROUND","PORRUA"];
+  favoritos: any[] = [
+    {Nombre:"4tools Power Systems",
+      Direccion:"666"},
+    {Nombre:"C-Estrategia",
+      Direccion:"El Inge"},
+    {Nombre:"SITAH",
+      Direccion:"666"},
+    {Nombre:"PTREE",
+      Direccion:"Dr. Nick ;)"},
+    {Nombre:"PRO PHARMA RESEARCH",
+      Direccion:"666"},
+    {Nombre:"EL REFORMA",
+      Direccion:"Universidad"},
+    {Nombre:"IFT",
+      Direccion:"666"},
+    {Nombre:"OXXO",
+      Direccion:"Estación UPIICSA"},
+    {Nombre:"7 ELEVEN",
+      Direccion:"Estacion Iztacalco"},
+    {Nombre:"Otro",
+      Direccion:"666"}];
 
   showSplash = true;
 
-  constructor(platform: Platform,
-    statusBar: StatusBar,
-    splashScreen: SplashScreen) {
+  constructor(platform: Platform,statusBar: StatusBar,splashScreen: SplashScreen,
+    private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -28,7 +50,8 @@ export class MyApp {
     });
   }
 
-  abrirFavorito(fav){
-
+  abrirFavorito(fav: any){
+    this.nav.push(this.favorito,{'fav':fav});
+    this.menuCtrl.close();
   }
 }
--- testing/src/pages/favorites/favorites.ts
+++ testing/src/pages/favorites/favorites.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, ModalController } from 'ionic-angular';
+import { IonicPage, NavController, ModalController,NavParams } from 'ionic-angular';
 import { DetailsPage } from '../details/details';
 import { Storage } from '@ionic/storage';
 import { ApiServiceProvider } from '../../providers/api-service/api-service';
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -10,22 +11,24 @@ import { ApiServiceProvider } from '../../providers/api-service/api-service';
   templateUrl: 'favorites.html',
 })
 export class FavoritesPage {
-
-  schools: any = [];
-  users: any[] = [];
+  root:any = HomePage;
+  arrayFavoritos:any={};
 
   constructor(
     public navCtrl: NavController,
     public modalCtrl: ModalController,
     public storage: Storage,
-    private api: ApiServiceProvider
-  ) {
-    this.storage.get('fav').then((val) => {
+    private api: ApiServiceProvider,
+    public navParams: NavParams) {
+    /*this.storage.get('fav').then((val) => {
       this.schools = val;
-    });
+    });*/
+    this.arrayFavoritos = this.navParams.get("fav");
+
   }
 
-  ionViewDidLoad(){
+
+  /*ionViewDidLoad(){
     this.api.getData().subscribe((data) =>{
       this.users = data['results'];
     }, (error) => {
@@ -36,6 +39,6 @@ export class FavoritesPage {
   details(a){
     let modal = this.modalCtrl.create(DetailsPage, { escuela: a, star: true });
     modal.present();
-  }
+  }*/
 
 }
