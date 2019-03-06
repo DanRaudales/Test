3ca691c5eb52b8e444948488f29fa5f192c4178b
Daniel Raudales <dan.raudales.26@gmail.com>
2019-03-05 23:33:55 -0600

    Favoritos y redireccionamiento
    Se agregó un botón para mostrar los favoritos pero si se filtran lo hace a todos los lugares no solo a los favoritos (revisar).
    
    El redireccionamiento en html es necesario hacer pruebas en móvil

--- testing/src/pages/home/home.ts
+++ testing/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, MenuController, ModalController, AlertController } from 'ionic-angular';
 import { ApiServiceProvider } from '../../providers/api-service/api-service';
 import { DetailsPage } from '../details/details';
+import { Storage } from '@ionic/storage';
 import { HttpClient } from '@angular/common/http';
 import * as Leaflet from 'leaflet';
 
@@ -27,21 +28,120 @@ export class HomePage {
   markerGroup: any; // Grupo (layer) de marcadores del mapa
   markers = []; // Array con los marcadores de los lugares
 
+  favoritos: any = [];
+  markerFavorites: any = [];
+  backupMarkers: any = [];
+  iconFavName = 'star-outline';
+
   constructor(
     public navCtrl: NavController,
     public menuCtrl: MenuController,
     public modalCtrl: ModalController,
     public http: HttpClient,
     public alertCtrl: AlertController,
-    public apiService: ApiServiceProvider
+    public apiService: ApiServiceProvider,
+    public storage: Storage
   ) {
+    this.storage.get('favoritos').then((data) => {
+      if (data) {
+        this.favoritos = data;
+        // Iterar en cada lugar que se recibe
+        this.favoritos.forEach(ss => {
+          let coor: any[] = [ss.COORDENADAS.lat, ss.COORDENADAS.lng]; // Crear un array con las coordenadas del lugar
+          // Añadir el lugar al array de los marcadores
+          this.markerFavorites.push(Leaflet.marker([coor[0], coor[1]]).bindPopup(ss.NOM_CORTO_PRESTATARIO).on('contextmenu', () => {
+            this.details(ss);
+          }))
+        });
+      }
+    });
+  }
 
+  alertFavorites() {
+    console.log(this.iconFavName)
+    // Validar que alerta se va a mostrar si para mostrar o para ocultar los favoritos
+    if (this.iconFavName == 'star-outline') {
+      // Alerta de confirmación para mostrar los favoritos
+      this.alertShowFavorites()
+    } else {
+      // Alerta de confirmación para ocultar los favoritos y mostrar los marcadores anteriores
+      this.alertHideFavorites()
+    }
+  }
+
+  // Confirmación para ocultar los favoritos y mostrar los marcadores anterirores
+  alertHideFavorites() {
+    let confirm = this.alertCtrl.create({
+      title: '¿Desea ocultar sus favoritos?',
+      message: 'Se mostraran los marcadores anteriores',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+            console.log('Ocultar favoritos');
+            // Iterar en los marcadores que se muestran
+            for (let i = 0; i < this.markers.length; i++) {
+              this.map.removeLayer(this.markers[i]) // Remover los marcadores del layer del mapa
+            }
+            this.markers = []; // Limpiar el array de los marcadores 
+            this.markers = this.backupMarkers; // Asignar los marcadores al array
+            this.backupMarkers = [] // Vaciar el backup de los marcadores
+            this.markerGroup = Leaflet.layerGroup(this.markers) // Crear un layerGroup para el mapa
+            this.map.addLayer(this.markerGroup) // Añadir el nuevo layer al mapa
+            this.iconFavName = 'star-outline'
+          }
+        },
+        {
+          text: 'Cancelar',
+          handler: () => {
+
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  // Confirmación para mostrar los favoritos
+  alertShowFavorites() {
+    let confirm = this.alertCtrl.create({
+      title: '¿Desea ver sus favoritos?',
+      message: 'Solo se verán sus favoritos en pantalla',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+            console.log('Mostrar solo favoritos');
+
+            // Crear un backup de los marcadores que se mostraban antes de los favoritos
+            this.backupMarkers = this.markers;
+
+            // Iterar en los marcadores que se muestran
+            for (let i = 0; i < this.markers.length; i++) {
+              this.map.removeLayer(this.markers[i]) // Remover los marcadores del layer del mapa
+            }
+            this.markers = []; // Limpiar el array de los marcadores 
+            this.showMarkers(this.favoritos); // Generar el nuevo array de los marcadores
+            this.markerGroup = Leaflet.layerGroup(this.markers) // Crear un layerGroup para el mapa
+            this.map.addLayer(this.markerGroup) // Añadir el nuevo layer al mapa
+            this.iconFavName = 'star'
+          }
+        },
+        {
+          text: 'Cancelar',
+          handler: () => {
+
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
   // Mostrar alerta de filtrado de programas academicos
   showAlert() {
     // Crear alerta
-    let alert = this.alertCtrl.create(); 
+    let alert = this.alertCtrl.create();
     // Agregar un titulo a la alerta
     alert.setTitle('Programa académico');
 
@@ -80,7 +180,7 @@ export class HomePage {
         console.log('Nuevo array')
         console.log(newPlaces)
         // Iterar en los marcadores que se muestran
-        for(let i = 0; i < this.markers.length; i++){
+        for (let i = 0; i < this.markers.length; i++) {
           this.map.removeLayer(this.markers[i]) // Remover los marcadores del layer del mapa
         }
         this.markers = []; // Limpiar el array de los marcadores 
@@ -108,7 +208,7 @@ export class HomePage {
     this.mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
 
     this.grayscale = Leaflet.tileLayer(this.mbUrl, { id: 'mapbox.light' }),
-    this.streets = Leaflet.tileLayer(this.mbUrl, { id: 'mapbox.streets' });
+      this.streets = Leaflet.tileLayer(this.mbUrl, { id: 'mapbox.streets' });
 
     this.showMarkers(this.places); // Generar el array con los marcadores
     this.markerGroup = Leaflet.layerGroup(this.markers) // Crear el layerGroup con los marcadores
@@ -122,7 +222,7 @@ export class HomePage {
 
     this.baseLayers = {
       "Streets": this.streets,
-      "Grayscale": this.grayscale
+      "Grayscale": this.grayscale,
     };
 
     Leaflet.control.layers(this.baseLayers).addTo(this.map)
@@ -160,10 +260,9 @@ export class HomePage {
     places.forEach(ss => {
       let coor: any[] = [ss.COORDENADAS.lat, ss.COORDENADAS.lng]; // Crear un array con las coordenadas del lugar
       // Añadir el lugar al array de los marcadores
-      this.markers.push(Leaflet.marker([coor[0], coor[1]]).bindPopup(ss.NOM_CORTO_PRESTATARIO).on('contextmenu', () => {
+      this.markers.push(Leaflet.marker([coor[0], coor[1]]).bindPopup('<a href="https://www.google.com" target="_blank">' + ss.NOM_CORTO_PRESTATARIO + '</a>').on('contextmenu', () => {
         this.details(ss);
       }))
-
     });
   }
 
